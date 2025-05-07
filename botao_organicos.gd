883656f0a69ead226d1cd80251e063d2cd8b01eb
RyuWk <raenier.ryu@gmail.com>
2025-05-06 23:29:34 -0400

    main
    main

---
+++ botao_organicos.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	pass
+func _process(delta):
+	pass
+
+func _on_button_pressed() -> void:
+	get_parent().lixoOrganixo()
+	pass # Replace with function body.
---
+++ scripts/fase_1.gd
@@ -0,0 +1,17 @@
+extends Node2D
+var organico = 0
+var vidro = 0
+var metal = 0
+var papel = 0
+var plastico = 0
+
+func _ready():
+	pass
+func _process(delta):
+	pass
+
+func Organico(organico):
+	organico += 1
+	if(organico<4):
+		
+		return
